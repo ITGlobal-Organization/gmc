637791b91b98ffa2ef55f310789d8241e7a4767b
Ghazanfar Ahsan <52095337+GhazanfarAhsan@users.noreply.github.com>
2023-10-18 22:10:46 +0500

    edit rule for blog added

--- config/rules.php
+++ config/rules.php
@@ -9,6 +9,14 @@ return [
             'publisher' => 'required',
             'publish_at' => 'required|date',
             'description' => 'required'
+        ],
+        'edit' => [
+            'title' => 'required',
+            'slug' => 'required',
+            'author' => 'required',
+            'publisher' => 'required',
+            'publish_at' => 'required|date',
+            'description' => 'required'
         ]
     ],
 
