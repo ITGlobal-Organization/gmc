c68b177a216b046a46e7d76d0853860921f2b7f5
AbdulRehman28 <abdulrehmanahmed20@gmail.com>
2023-11-02 23:54:21 +0500

    footer services commented

--- resources/views/footers/footerv2.blade.php
+++ resources/views/footers/footerv2.blade.php
@@ -2,21 +2,21 @@
 <div class="middle">
 
 
-<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mb-25">
+<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 mb-25">
 <h4>About us</h4>
 <div class="footer-bar"></div>
 <p class="p">
 Perthshire Chamber of Commerce (PCC) is an independent, non-profit making, non-political business membership organisation.
 Our focus is on championing the issues that matter to our members.
 </p>
-	
+
 </div>
-	
-	
-<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mb-25">
+
+
+{{-- <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 mb-25">
 <h4>Our Services</h4>
-<div class="footer-bar"></div>	
-	
+<div class="footer-bar"></div>
+
 <div id="list5">
     <ul>
       <li><a href="#">Graphics & Design</a></li>
@@ -27,16 +27,16 @@ Our focus is on championing the issues that matter to our members.
 	  <li><a href="#">Ecommerces</a></li>
     </ul>
     <div class="clr"></div>
-</div>	
-	
-<div class="clr"></div>	
 </div>
-	
-	
-<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mb-25">
+
+<div class="clr"></div>
+</div> --}}
+
+
+<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 mb-25">
 <h4>Useful Links</h4>
-<div class="footer-bar"></div>	
-	
+<div class="footer-bar"></div>
+
 <div id="list5">
     <ul>
       <li><a href="#">Home</a></li>
@@ -46,27 +46,27 @@ Our focus is on championing the issues that matter to our members.
       <li><a href="#">Contact</a></li>
     </ul>
     <div class="clr"></div>
-</div>	
-	
-<div class="clr"></div>	
 </div>
 
-<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mb-25">
+<div class="clr"></div>
+</div>
+
+<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 mb-25">
 <img src="{{ asset(config('site_config.assets.logo')) }}" width="100%" alt="" class="mb-25"/>
-	
+
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 border padding text-center">
 <h4>Stay Connected</h4>
-<span class="social-media1"> <a href="#"><i class="fab fa-facebook-f" title="Facebook"></i></a> </span> 
+<span class="social-media1"> <a href="#"><i class="fab fa-facebook-f" title="Facebook"></i></a> </span>
 <span class="social-media1"> <a href="#"><i class="fab fa-instagram" title="Instagram"></i></a> </span>
 <span class="social-media1"> <a href="#"><i class="fab fa-youtube" title="Youtube"></i></a> </span>
 <span class="social-media1"> <a href="#"><i class="fab fa-linkedin" title="Linkedin"></i></a> </span>
  <div class="clr"></div>
-</div>	
+</div>
 
-</div>	
-	
+</div>
+
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 padding text-center white">© Copyright 2023. All Right Reserved.</div>
-	
+
 <div class="clr"></div>
 </div>
 <div class="clr"></div>
