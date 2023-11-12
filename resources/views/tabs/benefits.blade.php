5b275bf42476c91b25043be6b698750f51cafe37
dinger1986 <daniel.lamb@flonix.co.uk>
2023-11-12 22:12:50 +0000

    Update benefits.blade.php

--- resources/views/tabs/benefits.blade.php
+++ resources/views/tabs/benefits.blade.php
@@ -1,21 +1,24 @@
 <!--Start Benefits Tab Area-->
-<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-    <div class="main-box border" style="text-align: center;">
-        <div class="box-img card-image" style="padding-top: 50px;">
-                            <a href="https://www.flonix.co.uk/" style="display: block;">
-                                    <img src="/media/barnd5.png" alt="" style="max-width: 150px; width: 100%; height: auto; margin: auto; display: block;"/>
-                                    </a>
-                                    </div>
-                                        <div class="box-name">
-                                            <a href="https://www.flonix.co.uk/" style="color: #ffffff; text-decoration: none; font-size: 24px; background-color: #609897; padding: 5px; display: inline-block;">Flonix Ltd</a>
-                                        </div>
-                                            <div class="box-text">
-                                        Flonix Ltd offers a free Pen Test and report for Members.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <div class="clr"></div>
+<div class="benefits-tab">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 border margin-tb25">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="main-box border" style="text-align: center;">
+          <div class="box-img card-image" style="padding-top: 50px;">
+            <a href="https://www.flonix.co.uk/" style="display: block;">
+                <img src="/media/barnd5.png" alt="" style="max-width: 150px; width: 100%; height: auto; margin: auto; display: block;"/>
+             </a>
             </div>
- <!--End Benefits Tab Area-->
+              <div class="box-name">
+             <a href="https://www.flonix.co.uk/" style="color: #ffffff; text-decoration: none; font-size: 24px; background-color: #609897; padding: 5px; display: inline-block;">Flonix Ltd</a>
+             </div>
+         <div class="box-text">
+            Flonix Ltd offers a free Pen Test and report for Members.
+        </div>
+       </div>
+     </div>
+    </div>
+  </div>
+   <div class="clr"></div>
+  </div>
+<!--End Benefits Tab Area-->
