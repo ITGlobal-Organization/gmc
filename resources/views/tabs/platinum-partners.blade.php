91b8b598c6de564002e593c7ec9538248c3e0637
Ghazanfar Ahsan <52095337+GhazanfarAhsan@users.noreply.github.com>
2024-01-04 00:46:16 +0500

    Platinum Partners url issue

--- resources/views/tabs/platinum-partners.blade.php
+++ resources/views/tabs/platinum-partners.blade.php
@@ -9,12 +9,12 @@
                         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                             <div class="main-box">
                                 <div class="box-img card-image">
-                                    <a href="{{ $PlatinumPartner->web_url != '' ?  $PlatinumPartner->web_url : 'javascript:void(0)'}}"><img src="{{ $PlatinumPartner->image_url }}"
+                                    <a href="{ route('platinum-partners.get',$PlatinumPartner->slug) }}"><img src="{{ $PlatinumPartner->image_url }}"
                                             alt="" width="100%" class="box-img" /></a>
                                 </div>
                                 <div class="box-name home">
                                     <div style="display: flex; justify-content: center; align-items: center;">
-                                        <a href="{{ $PlatinumPartner->web_url != '' ?  $PlatinumPartner->web_url : 'javascript:void(0)'}}"
+                                        <a href="{ route('platinum-partners.get',$PlatinumPartner->slug) }}"
                                             style="color: #ffffff; text-decoration: none; font-size: 14px; padding: 5px;">
                                             {{ $PlatinumPartner->title }}
                                         </a>
