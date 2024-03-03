bf5191a30fabcccc11952197ef244b6b6f9c51a6
AbdulRehman28 <abdulrehmanahmed20@gmail.com>
2024-03-04 03:13:01 +0500

    changes in registration

--- app/Http/Controllers/ZohoController.php
+++ app/Http/Controllers/ZohoController.php
@@ -28,7 +28,8 @@ class ZohoController extends BaseController
     private $user,$Media,$authUser;
     private  $zohoAuthToken = '1000.1baf1559dd09adcc65ebcc2ffff02fc0.675dec9e4a1a83b5501847c2640590e6';
     private $refreshToken = '1000.dbf2e2f5d89607e359667ad4db6c0afe.14af10121266ce8d2ab7436d1fc283e1';
-    private $leadData;
+    private $leadData,$directoryId,$requestData,$userData;
+
 
     public function __construct(User $user,Directory $directory){
         $this->user = $user;
@@ -38,11 +39,13 @@ class ZohoController extends BaseController
         $this->setModel( $this->directory);
         $this->client = new \GuzzleHttp\Client();
         $this->authUser;
+        $this->directoryId;
+        $this->requestData;
 
     }
 
     public function register(Request $request){
-
+        // $this->zohoAuthToken = DB::table('tokens')->where('id', '=', 1)->pluck('access_token')[0];
         $request->validate($this->user->getRules());
         $request->validate($this->directory->getRules());
         if(!isset($request->category_ids)){
@@ -62,18 +65,23 @@ class ZohoController extends BaseController
         $request->merge(["user_id"=>$user,"mobile_no"=>$request->tel_no]);
         $directoryData = $request->except(['_token','password_confirmation','password','company','category_ids','postalcode','tel_no','website']);
         $directory = $this->directory->store($directoryData);
+        $this->directoryId = $directory;
+
         foreach($request->category_ids as $Category){
             $this->directory->addRelTableRecord($Category,'category_directory','category_id');
         }
+        $this->requestData = $request;
 
-        $response = $this->registerLead($request);
+
+        $response = $this->registerLead($this->requestData);
 
         if($response['status'] != true){
-            $this->user->where('id',$this->authUser)->delete();
+            $this->user->where('id',$this->authUser->id)->delete();
         }else{
+            // dd($response);
             $response = $this->registerUser($request);
             if($response['status'] != true){
-                $this->user->where('id',$this->authUser)->delete();
+                $this->user->where('id',$this->authUser->id)->delete();
             }
         }
 
@@ -101,11 +109,39 @@ class ZohoController extends BaseController
                 ]
             ]
         ];
+        $response= $this->leadApi($this->leadData);
 
-        $response = $this->createLead();
+        $response = json_decode($response->getBody(),true);
 
         return $this->checkResponse($response);
     }
+    public function checkResponse($response){
+        if (isset($response['data'][0]['code']) && $response['data'][0]['code'] == "SUCCESS"){
+            $id = $response['data'][0]['details']['id'];
+            $this->updateZohoId($id,"directory");
+            $response = $this->Response(true,$id,'',$response);
+            return $response;
+
+        }else if ($response['code'] && ($response['code'] == "AUTHENTICATION_FAILURE" || $response['code'] == "INVALID_TOKEN")){
+            $response = $this->updateToken();
+
+            if($response == true){
+                $response = $this->leadApi($this->leadData);
+                $response = json_decode($response->getBody(),true);
+                $id = $response['data'][0]['details']['id'];
+                $this->updateZohoId($id,"directory");
+                $response = $this->Response(true,$id,'',$response);
+                return $response;
+            }else{
+                return false;
+            }
+
+        }else{
+            return false;
+        }
+    }
+
+
     public function registerUser($request){
 
         $this->userData = [
@@ -119,96 +155,40 @@ class ZohoController extends BaseController
             ]
             ]
         ];
+        $response= $this->userApi($this->userData);
 
-    $response = $this->createUser();
-
-    return $this->checkResponse($response);
-}
-
-    public function createLead(){
-
-       $response= Http::withBody(json_encode($this->leadData), 'application/json')
-        ->withHeaders([
-            'Authorization' => 'Zoho-oauthtoken '.$this->zohoAuthToken,
-            'Scope'=>'ZohoCRM.modules.ALL'
-        ])
-        ->post('https://www.zohoapis.eu/crm/v2/Leads');
         $response = json_decode($response->getBody(),true);
 
-        return $response;
 
-
-    }
-
-    public function createUser(){
-
-
-        $response= Http::withBody(json_encode($this->userData), 'application/json')
-         ->withHeaders([
-            'Authorization' => 'Zoho-oauthtoken '.$this->zohoAuthToken,
-             'Scope'=>'ZohoCRM.modules.ALL'
-         ])
-         ->post('https://www.zohoapis.eu/crm/v2/Contacts');
-
-         $response = json_decode($response->getBody(),true);
-
-         return $response;
-
-
-     }
-
-
-    public function checkResponse($response){
-
+    return $this->checkUserResponse($response);
+}
+    public function checkUserResponse($response){
         if (isset($response['data'][0]['code']) && $response['data'][0]['code'] == "SUCCESS"){
-
             $id = $response['data'][0]['details']['id'];
-            Auth::login($this->authUser);
-            return $this->Response(true,$id,'',$response);
+            $this->updateZohoId($id,"user");
+            $response = $this->Response(true,$id,'',$response);
+            return $response;
 
-        }
+        }else if ($response['code'] && ($response['code'] == "AUTHENTICATION_FAILURE" || $response['code'] == "INVALID_TOKEN")){
+            $response = $this->updateToken();
 
-        if(isset($response['status']) && $response['status'] == "error"){
-
-            if(($response['code']) && ($response['code'] == "AUTHENTICATION_FAILURE" || $response['code'] == "INVALID_TOKEN")){
-
-                $response = $this->getToken();
-
-                if(isset($response['access_token'])){
-
-                    $this->zohoAuthToken = $response['access_token'];
-                    $response = $this->createLead();
-                    if (isset($response['data'][0]['code']) && $response['data'][0]['code'] == "SUCCESS"){
-
-                        $id = $response['data'][0]['details']['id'];
-                        Auth::login($this->authUser);
-                        return $this->Response(true,$id,'',$response);
-
-                    }else{
-                        return $this->Response(false,'','Unknown error.',$response);
-
-                    }
-
-                }else{
-                    return $this->Response(false,'','Refresh token is invalid.',$response);
-
-                }
-
-            }else if(isset($response['error']) && $response['error'] == "invalid_code"){
-                return $this->Response(false,'','Token is invalid.',$response);
-
+            if($response == true){
+                $response = $this->userApi($this->userData);
+                $response = json_decode($response->getBody(),true);
+                $id = $response['data'][0]['details']['id'];
+                $this->updateZohoId($id,"user");
+                $response = $this->Response(true,$id,'',$response);
+                return $response;
             }else{
-                return $this->Response(false,'','Unknown error.',$response);
-
+                return false;
             }
 
         }else{
-            return $this->Response(false,'','Could not create lead.',$response);
-
+            return false;
         }
     }
 
-    public function getToken(){
+    public function updateToken(){
 
         $response= Http::asForm()
                 ->post('https://accounts.zoho.eu/oauth/v2/token', [
@@ -219,19 +199,52 @@ class ZohoController extends BaseController
                     'redirect_uri'=>'http://gmc.test'
                 ]);
         $response = json_decode($response->getBody(),true);
-        return $response;
+        if(isset($response['access_token'])){
+            $this->zohoAuthToken = $response['access_token'];
+            // DB::table('tokens')->where('id', '=', 1)->update(['access_token'=>$this->zohoAuthToken]);
+            return true;
+        }
+        return false;
     }
 
     public function Response($status,$id='',$error='',$response){
 
-        $response = [
+        $responseArr = [
             'status'=>$status,
             'id'=>$id,
             'error'=>$error,
             'message'=>$response
         ];
-        return $response;
 
+        return $responseArr;
+
+    }
+    public function updateZohoId($id,$table){
+        if($table == "directory"){
+            $this->directory->where('id',$this->directoryId)->update(['zoho_id'=>$id,'is_approved'=>0]);
+        }else if($table == "user"){
+            $this->user->where('id',$this->authUser->id)->update(['zoho_id'=>$id]);
+            Auth::login($this->authUser);
+        }
+        return $id;
+    }
+    public function leadApi($leadData){
+        $response = Http::withBody(json_encode($this->leadData), 'application/json')
+        ->withHeaders([
+            'Authorization' => 'Zoho-oauthtoken '.$this->zohoAuthToken,
+            'Scope'=>'ZohoCRM.modules.ALL'
+        ])
+        ->post('https://www.zohoapis.eu/crm/v2/Leads');
+        return $response;
+    }
+    public function userApi($userData){
+        $response = Http::withBody(json_encode($this->userData), 'application/json')
+        ->withHeaders([
+            'Authorization' => 'Zoho-oauthtoken '.$this->zohoAuthToken,
+            'Scope'=>'ZohoCRM.modules.ALL'
+        ])
+        ->post('https://www.zohoapis.eu/crm/v2/Contacts');
+        return $response;
     }
 }
 
