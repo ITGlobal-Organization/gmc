3e5a6f7ef28d84dd96f3c4b32210f8046d510c24
Ghazanfar Ahsan <52095337+GhazanfarAhsan@users.noreply.github.com>
2024-03-18 02:22:50 +0500

    forget passwod customized successfully

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -29,7 +29,7 @@
     <link href="{{ custom_asset('owl.theme.css','css') }}" rel="stylesheet" type="text/css">
 
 	<link href="{{ custom_asset('easy-responsive-tabs.css','css') }}" rel="stylesheet" type="text/css">
-
+	<link rel="manifest" href="{{ asset('manifest.json') }}" />
 
 	<!-- <script src="{{ asset(config('site_config.assets.plugins').'jquery/jquery.js') }}"></script>  -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -54,7 +54,7 @@
 <!--End Header-->
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 border padding">
 @include('components.loader')
-<audio class="message-notification" src="{{ asset(config('site_config.assets.sounds'). 'message.mp3') }}" autoplay></audio>
+<audio class="message-notification" src="{{ asset(config('site_config.assets.sounds'). 'message.mp3') }}"></audio>
 </div>
 
 
