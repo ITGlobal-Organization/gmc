793506da3bbaebd008f143ec55a531d7da94de02
AbdulRehman28 <abdulrehmanahmed20@gmail.com>
2024-04-04 20:14:27 +0500

    test 4

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -51,11 +51,12 @@ class AuthenticatedSessionController extends BaseController
         $request->session()->regenerate();
 
 
-        $device = Agent::device();
-        $savedDevice = DB::table('devices')->where('user_id',auth()->user()->id)->where('device',$device)->where('is_otp_validated',1)->first();
-        if($savedDevice != ""){
-            auth()->user()->is_login = 1;
-        }
+        $device = $request->userAgent();
+        dd($device);
+        // $savedDevice = DB::table('devices')->where('user_id',auth()->user()->id)->where('device',$device)->where('is_otp_validated',1)->first();
+        // if($savedDevice != ""){
+        //     auth()->user()->is_login = 1;
+        // }
         auth()->user()->login_at = Carbon::now();
         auth()->user()->save();
         $user = auth()->user();
