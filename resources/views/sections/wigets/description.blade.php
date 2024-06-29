c5e40834a93629c607ddf3c4f4dfa8e485b9fc49
ghazanfar.ahsan <ghazanfar.ahsan@tpsonline.com>
2024-06-29 13:07:18 +0500

    removing tags

--- resources/views/sections/wigets/description.blade.php
+++ resources/views/sections/wigets/description.blade.php
@@ -1,8 +1,8 @@
 <div class="box-text">
                     @php 
-                        //$Description = strip_tags($Description);
-                        $Description = preg_replace('/<[^>]+>/', '', $Description);
-                        //$Description = htmlspecialchars($Description, ENT_QUOTES, 'UTF-8');
+                        $Description = strip_tags($Description);
+                        //$Description = preg_replace('/<[^>]+>/', '', $Description);
+                        $Description = htmlspecialchars($Description, ENT_QUOTES, 'UTF-8');
                     
                     @endphp 
         {{ $Description }}
